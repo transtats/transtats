e4daabf6a62f8294b9355ec39959a715873e1a6f
Sachin Pathare <spathare@redhat.com>
2017-05-31 09:33:15 +0530

    added envrionment variable in dockerfile

--- deploy/docker/launch.sh
+++ deploy/docker/launch.sh
@@ -5,10 +5,10 @@ export PYTHONPATH=/workspace;$PYTHONPATH
 
 # setup db
 su - postgres -c "pg_ctl -D /var/lib/pgsql/data -l logfile start" && sleep 5
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
-sudo -u postgres psql -c "CREATE DATABASE transtats;"
+sudo -u postgres psql -c "ALTER USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASSWD';"
+sudo -u postgres psql -c "CREATE DATABASE $DATABASE_NAME;"
 make migrate
-su - postgres -c "psql -d transtats -a -f /workspace/deploy/docker/data/initial.sql"
+su - postgres -c "psql -d $DATABASE_NAME -a -f /workspace/deploy/docker/data/initial.sql"
 
 # setup app
 python3 manage.py initlogin && make demo
