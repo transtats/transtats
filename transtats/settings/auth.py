e176999ce6683fab23aad3a576b51d739dfbc824
sundeep-co-in <suanand@redhat.com>
2017-12-20 15:34:58 +0530

    oidc jwk2pem

--- transtats/settings/auth.py
+++ transtats/settings/auth.py
@@ -24,6 +24,5 @@ OIDC_OP_AUTHORIZATION_ENDPOINT = 'https://iddev.fedorainfracloud.org/openidc/Aut
 OIDC_OP_TOKEN_ENDPOINT = 'https://iddev.fedorainfracloud.org/openidc/Token'
 OIDC_OP_USER_ENDPOINT = 'https://iddev.fedorainfracloud.org/openidc/UserInfo'
 OIDC_RP_SIGN_ALGO = 'RS256'
-# fix OIDC_RP_IDP_SIGN_KEY to use latest mozilla-django-oidc
-OIDC_RP_IDP_SIGN_KEY = {"use": "sig", "kid": "1462960685-sig", "e": "AQAB", "kty": "RSA", "n": "q_0_XjILQxF3OaQZtFE3wVJ5UUuxZbxiJ_z-Zai0EOHiaMMxVyooibDRen615r525DQ8TmQyR0eMQEpQ6SUvaOunahpYohgAkbkYggUMQhcoCLme18ZJBTNWTP8w4t7mcuZd1cy1KtHpEvH4gkrjp8N3vIv1lzFraSc-p2rHMbV-AX5CJQ1HohBdwaqyOBKp0nzY27gu2EH2vzCwXkO4zGtrHfjjGc0Ra4WG-xz1AWg833xcFj3pqM3vca09jDLBme-GT151LcCCXRNyOZPZ3ZX62NxkMyqvVJHC3Uu2Q1hSHO7f6AZkZXY88PXXEH52T2ZrWiISowjTcGUboP8goQ"}
+OIDC_RP_IDP_SIGN_KEY = '-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAq/0/XjILQxF3OaQZtFE3wVJ5UUuxZbxiJ/z+Zai0EOHiaMMxVyoo\nibDRen615r525DQ8TmQyR0eMQEpQ6SUvaOunahpYohgAkbkYggUMQhcoCLme18ZJ\nBTNWTP8w4t7mcuZd1cy1KtHpEvH4gkrjp8N3vIv1lzFraSc+p2rHMbV+AX5CJQ1H\nohBdwaqyOBKp0nzY27gu2EH2vzCwXkO4zGtrHfjjGc0Ra4WG+xz1AWg833xcFj3p\nqM3vca09jDLBme+GT151LcCCXRNyOZPZ3ZX62NxkMyqvVJHC3Uu2Q1hSHO7f6AZk\nZXY88PXXEH52T2ZrWiISowjTcGUboP8goQIDAQAB\n-----END RSA PUBLIC KEY-----\n'
 LOGOUT_REDIRECT_URL = '/'
