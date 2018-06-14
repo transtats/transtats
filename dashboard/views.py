2e16da04decd08f9f5799b7b3243ac6d963f58e5
Bhavin Gandhi <bhavin192@redhat.com>
2018-06-14 16:16:51 +0530

    [UI] Add edit package section
    - Possible to edit upstream_url, transplatform_slug, release_streams, release_branch_mapping
    - Validates if the package exist on the selected translation platforms
    
    Signed-off-by: Bhavin Gandhi <bhavin192@redhat.com>

--- dashboard/views.py
+++ dashboard/views.py
@@ -40,7 +40,7 @@ from dashboard.constants import (
     APP_DESC, TS_JOB_TYPES, RELSTREAM_SLUGS, TRANSPLATFORM_ENGINES
 )
 from dashboard.forms import (
-    NewPackageForm, NewReleaseBranchForm, NewGraphRuleForm,
+    NewPackageForm, UpdatePackageForm, NewReleaseBranchForm, NewGraphRuleForm,
     NewLanguageForm, UpdateLanguageForm, LanguageSetForm,
     NewTransPlatformForm, UpdateTransPlatformForm
 )
@@ -56,7 +56,7 @@ from dashboard.managers.graphs import (
     GraphManager, ReportsManager
 )
 from dashboard.models import (
-    Jobs, Languages, LanguageSet, TransPlatform, Visitor
+    Jobs, Languages, LanguageSet, TransPlatform, Visitor, Packages
 )
 
 
@@ -445,6 +445,20 @@ class NewPackageView(ManagersMixin, FormView):
                 ))
             return HttpResponseRedirect(self.get_success_url())
         return render(request, self.template_name, {'form': form, 'POST': 'invalid'})
+
+
+class UpdatePackageView(SuccessMessageMixin, UpdateView):
+    """
+    Update Package view
+    """
+    template_name = 'packages/package_update.html'
+    model = Packages
+    slug_field = 'package_name'
+    form_class = UpdatePackageForm
+    success_message = '%(package_name)s was updated successfully!'
+
+    def get_success_url(self):
+        return reverse('package-update', args=[self.object.package_name])
 
 
 class GraphRulesSettingsView(ManagersMixin, ListView):
