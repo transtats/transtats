62a7bf9a308ff3e90686d3689113aedc5c2c35fa
Bhavin Gandhi <bhavin7392@gmail.com>
2018-12-16 18:59:18 +0530

    [UI] Update PatternFly version to 3.58.0
    - This also changes the paths used for js files
    - Adds jquery.flot (0.8.3), jquery.flot.tooltip (0.9.0) as dependency
      for Transtats
    
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- transtats/settings/base.py
+++ transtats/settings/base.py
@@ -165,8 +165,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # TODO: find some method to serve only specific files downloaded from npm
-    os.path.join(BASE_DIR, "node/node_modules/patternfly/"),
+    # TODO: find some method to serve only specific files downloaded from npm possible solutions gulp, grunt
+    os.path.join(BASE_DIR, "node/node_modules/"),
     os.path.join(BASE_DIR, "static"),
 )
 
