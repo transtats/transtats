98d4a99764c3a9e7f41736fbb9289519ef53360d
Bhavin Gandhi <bhavin7392@gmail.com>
2018-12-16 18:47:12 +0530

    [UI] Modify base.html to use PatternFly
    - One JS workaround to make the about modal work on mobile devices
    - Have proper ids to elements, <element-type>-<name> i.e. button-user
    - Using npm to install PatternFly and it's dependencies
    - Add make ui-deps which runs npm install
    - Run ui-deps for make static as well
    - Download the node_modules inside transtats/node, package.json is also present there
    - Modify transtats.css to have different background color and extra bottom margin
    
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- transtats/settings/base.py
+++ transtats/settings/base.py
@@ -165,6 +165,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    # TODO: find some method to serve only specific files downloaded from npm
+    os.path.join(BASE_DIR, "node/node_modules/patternfly/"),
     os.path.join(BASE_DIR, "static"),
 )
 
