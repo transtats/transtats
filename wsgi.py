090b5775a452cf746b7a419ed12fcb856d4ff7ea
Sundeep Anand <suanand@redhat.com>
2019-03-13 19:21:12 +0530

    fix wsgi

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transtats.settings.test")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transtats.settings.test")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'transtats.settings.test'
 
 application = get_wsgi_application()
