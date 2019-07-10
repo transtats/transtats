371cbe97078bf0a03ddb9e081b64a8080396a03a
Sundeep Anand <suanand@redhat.com>
2019-07-10 17:33:19 +0530

    buildsys sync task

--- dashboard/management/commands/syncpackages.py
+++ dashboard/management/commands/syncpackages.py
@@ -13,28 +13,37 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import os
 import time
 import threading
+import yaml
 from datetime import timedelta
 
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
+from dashboard.constants import TS_JOB_TYPES, BRANCH_MAPPING_KEYS
 from dashboard.managers.packages import PackagesManager
-from dashboard.managers.graphs import ReportsManager
+from dashboard.managers.graphs import (GraphManager, ReportsManager)
+from dashboard.managers.jobs import (
+    JobTemplateManager, YMLBasedJobManager
+)
 
 
 class Command(BaseCommand):
 
     help = 'Sync packages with their respective translation platform.'
 
+    graph_manager = GraphManager()
     package_manager = PackagesManager()
     reports_manager = ReportsManager()
+    job_template_manager = JobTemplateManager()
 
     def _sync_package(self, pkg):
         self.package_manager.sync_update_package_stats(pkg)
 
-    def handle(self, *args, **options):
+    def sync_with_platform(self):
+
         all_packages = self.package_manager.get_packages().filter(
             platform_last_updated__lte=timezone.now() - timedelta(hours=6)
         ).order_by('platform_url')
@@ -48,3 +57,120 @@ class Command(BaseCommand):
 
         self.reports_manager.analyse_releases_status()
         self.reports_manager.analyse_packages_status()
+
+    def _update_diff(self, package):
+        try:
+            package_stats = self.graph_manager.get_trans_stats_by_package(
+                package.package_name
+            )
+            self.graph_manager.package_manager.calculate_stats_diff(
+                package.package_name, package_stats,
+                package.release_branch_mapping_json
+            )
+        except Exception:
+            # pass for now
+            pass
+
+    def _sync_build_system(self, template, params):
+
+        t_params = template.job_template_params
+        if len(t_params) == len(params):
+            job_data = {field.upper(): param
+                        for field, param in zip(t_params, params)}
+            job_data.update({
+                'YML_FILE': yaml.dump(template.job_template_json,
+                                      default_flow_style=False).replace("\'", "")
+            })
+            job_data.update({'SCRATCH': True})
+
+            temp_path = 'false/{0}/'.format('-'.join(params))
+            job_manager = YMLBasedJobManager(
+                **job_data, **{'params': [p.upper() for p in t_params],
+                               'type': TS_JOB_TYPES[3]},
+                **{'active_user_email': 'system@transtats.org'},
+                **{'sandbox_path': temp_path},
+                **{'job_log_file': temp_path + '.log'}
+            )
+
+            try:
+                os.mkdir(temp_path)
+                job_manager.execute_job()
+            except Exception as e:
+                # pass for now
+                pass
+            finally:
+                os.rmdir(temp_path)
+
+    def sync_with_build_system(self):
+
+        job_template = None
+        all_packages = self.package_manager.get_packages().filter(
+            release_branch_mapping__isnull=False
+        )
+        job_templates = self.job_template_manager.get_job_templates(
+            job_template_type=TS_JOB_TYPES[3]
+        )
+        if job_templates:
+            job_template = job_templates.first()
+
+        if all_packages and job_template:
+            for package in all_packages:
+                candidates = []
+                mapping = package.release_branch_mapping_json or {}
+
+                for release, map_dict in mapping.items():
+                    candidates.append((
+                        package.package_name,
+                        map_dict.get(BRANCH_MAPPING_KEYS[1]),
+                        map_dict.get(BRANCH_MAPPING_KEYS[2])
+                    ))
+
+                for candidate in candidates:
+                    th = threading.Thread(
+                        target=self._sync_build_system,
+                        args=(job_template, candidate,)
+                    )
+                    th.start()
+                    th.join()
+                    time.sleep(2)
+
+                self._update_diff(package)
+
+        self.reports_manager.analyse_packages_status()
+
+    def add_arguments(self, parser):
+
+        # Named (optional) arguments
+        parser.add_argument(
+            '--platform',
+            action='store_true',
+            help='Sync packages with translation platform only.',
+        )
+
+        parser.add_argument(
+            '--build-system',
+            action='store_true',
+            help='Sync packages with build system only.',
+        )
+
+    def handle(self, *args, **options):
+
+        cmd_combinations_options = [
+            'platform',
+            'build_system',
+            'default_both'
+        ]
+
+        cmd_combinations = {
+            cmd_combinations_options[0]: self.sync_with_platform,
+            cmd_combinations_options[1]: self.sync_with_build_system,
+            cmd_combinations_options[2]: [self.sync_with_platform,
+                                          self.sync_with_build_system]
+        }
+
+        if options.get(cmd_combinations_options[0]):
+            cmd_combinations.get(cmd_combinations_options[0])()
+        elif options.get(cmd_combinations_options[1]):
+            cmd_combinations.get(cmd_combinations_options[1])()
+        else:
+            [m() for m in cmd_combinations.get(cmd_combinations_options[2])]
--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -13,9 +13,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import os
 import time
 import threading
 from datetime import timedelta
+import yaml
 
 from celery.schedules import crontab
 from celery.task import periodic_task
@@ -23,8 +25,10 @@ from celery.utils.log import get_task_logger
 
 from django.utils import timezone
 
+from dashboard.constants import TS_JOB_TYPES, BRANCH_MAPPING_KEYS
 from dashboard.managers.packages import PackagesManager
-from dashboard.managers.graphs import ReportsManager
+from dashboard.managers.jobs import JobTemplateManager, YMLBasedJobManager
+from dashboard.managers.graphs import GraphManager, ReportsManager
 
 
 logger = get_task_logger(__name__)
@@ -60,3 +64,98 @@ def task_sync_packages_with_platform():
     reports_manager.analyse_releases_status()
     reports_manager.analyse_packages_status()
     logger.info("%s Packages sync'd with Translation Platform" % len(all_packages))
+
+
+@periodic_task(
+    run_every=(crontab(minute=0, hour='11,23')),
+    name="sync_packages_with_build_system",
+    ignore_result=True
+)
+def task_sync_packages_with_build_system():
+    """
+    sync all packages with build system
+    """
+
+    package_manager = PackagesManager()
+    graph_manager = GraphManager()
+    reports_manager = ReportsManager()
+    job_template_manager = JobTemplateManager()
+
+    def _update_diff(package):
+        try:
+            package_stats = graph_manager.get_trans_stats_by_package(
+                package.package_name
+            )
+            graph_manager.package_manager.calculate_stats_diff(
+                package.package_name, package_stats,
+                package.release_branch_mapping_json
+            )
+        except Exception:
+            # pass for now
+            pass
+
+    def _sync_build_system(template, params):
+
+        t_params = template.job_template_params
+        if len(t_params) == len(params):
+            job_data = {field.upper(): param
+                        for field, param in zip(t_params, params)}
+            job_data.update({
+                'YML_FILE': yaml.dump(template.job_template_json,
+                                      default_flow_style=False).replace("\'", "")
+            })
+            job_data.update({'SCRATCH': True})
+
+            temp_path = 'false/{0}/'.format('-'.join(params))
+            job_manager = YMLBasedJobManager(
+                **job_data, **{'params': [p.upper() for p in t_params],
+                               'type': TS_JOB_TYPES[3]},
+                **{'active_user_email': 'system@transtats.org'},
+                **{'sandbox_path': temp_path},
+                **{'job_log_file': temp_path + '.log'}
+            )
+
+            try:
+                os.mkdir(temp_path)
+                job_manager.execute_job()
+            except Exception as e:
+                # pass for now
+                pass
+            finally:
+                os.rmdir(temp_path)
+
+    job_template = None
+    all_packages = package_manager.get_packages().filter(
+        release_branch_mapping__isnull=False
+    )
+    job_templates = job_template_manager.get_job_templates(
+        job_template_type=TS_JOB_TYPES[3]
+    )
+    if job_templates:
+        job_template = job_templates.first()
+
+    if all_packages and job_template:
+        for package in all_packages:
+            candidates = []
+            mapping = package.release_branch_mapping_json or {}
+
+            for release, map_dict in mapping.items():
+                candidates.append((
+                    package.package_name,
+                    map_dict.get(BRANCH_MAPPING_KEYS[1]),
+                    map_dict.get(BRANCH_MAPPING_KEYS[2])
+                ))
+
+            for candidate in candidates:
+                th = threading.Thread(
+                    target=_sync_build_system,
+                    args=(job_template, candidate,)
+                )
+                th.start()
+                th.join()
+                time.sleep(2)
+
+            _update_diff(package)
+
+    reports_manager.analyse_packages_status()
+    logger.info("%s Packages sync'd with Build System" % len(all_packages))
