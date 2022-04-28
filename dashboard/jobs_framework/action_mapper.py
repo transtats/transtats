05a690f7f028b66eddb83c9efc8581344c8492e8
Sundeep Anand <suanand@redhat.com>
2022-04-28 12:36:30 +0530

    add desc for Pipeline Actions fix: External control of file name or path
    Signed-off-by: Sundeep Anand <suanand@redhat.com>

--- dashboard/jobs_framework/action_mapper.py
+++ dashboard/jobs_framework/action_mapper.py
@@ -69,6 +69,8 @@ class JobCommandBase(BaseManager):
         return log_text
 
     def _log_task(self, log_f, subject, text, text_prefix=None):
+        if ".log" not in log_f:
+            raise Exception("Log file is not formatted.")
         with open(log_f, 'a+') as the_file:
             text_to_write = \
                 '\n<b>' + subject + '</b> ...\n%s\n' % \
